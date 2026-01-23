d8071d463c47fb015411145fa7b0f777fc1de2cc
cd player <deepocean.2900@gmail.com>
2026-01-23 16:43:28 -0500

    switched to static imports for the translation files.

--- apps/client/src/lib/i18n.ts
+++ apps/client/src/lib/i18n.ts
@@ -1,15 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import resourcesToBackend from "i18next-resources-to-backend";
+import en from "../locales/en.json";
+import zh from "../locales/zh.json";
 
 i18n
-	.use(
-		resourcesToBackend(
-			(language: string, _namespace: string) =>
-				import(`../locales/${language}.json`),
-		),
-	)
 	.use({
 		type: "languageDetector",
 		async: true,
@@ -37,6 +32,14 @@ i18n
 	})
 	.use(initReactI18next)
 	.init({
+		resources: {
+			en: {
+				translation: en,
+			},
+			zh: {
+				translation: zh,
+			},
+		},
 		fallbackLng: "en",
 		debug: false,
 		interpolation: {
